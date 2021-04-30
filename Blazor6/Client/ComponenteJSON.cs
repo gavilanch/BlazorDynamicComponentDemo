1d5136fe70edd33ad1d1564eeee469cb4ca826df
Felipe Gavilan <elmejor_887@hotmail.com>
2021-04-30 10:52:32 -0400

    Initial Commit

---
+++ Blazor6/Client/ComponenteJSON.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor6.Client
+{
+public class ComponenteJSON
+{
+    public string Componente { get; set; }
+    public IDictionary<string, object> Parametros { get; set; }
+}
+
+public class JSONComponent
+{
+    public string Component { get; set; }
+    public IDictionary<string, object> Parameters { get; set; }
+}
+}
